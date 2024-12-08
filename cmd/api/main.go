94b4a7acd33128c4c8d69af9d069b2e20237befe
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-12-08 15:57:54 +0700

    feat: add healthcheck route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "environment", "development", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthCheckHandler)
+
+	server := http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      mux,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", server.Addr, "env", cfg.env)
+
+	err := server.ListenAndServe()
+
+	logger.Error(err.Error())
+	os.Exit(1)
 }
