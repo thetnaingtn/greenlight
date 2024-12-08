4e9fb5c324aad7f68c612672c84fd9fb3ecce761
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-12-08 16:15:52 +0700

    feat: helper to extract id

---
+++ cmd/api/helper.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+
+	return id, err
+}
