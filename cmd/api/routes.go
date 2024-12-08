52532e61fca40a4ad0afba2b7028dfff502461ad
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-12-08 16:11:30 +0700

    feat: add movies handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,8 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	return router
 }
