3f08977a75070b4c4392aa718707e230d7ee5ca9
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-12-10 21:12:22 +0700

    feat: add health check handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,12 +21,6 @@ type application struct {
 	logger *slog.Logger
 }
 
-func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
-}
-
 func main() {
 	var cfg config
 
