f17e3de34bc79225f8c8674b5ad216ab0f05153e
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2024-12-22 14:45:44 +0700

    feat: customize decoding for runtime field

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year,omitempty"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres,omitempty"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year,omitempty"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres,omitempty"`
 	}
 
 	err := app.readJSON(w, r, &input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	runtime = strconv.Quote(runtime)
 
 	return []byte(runtime), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+
+	unquoteStr, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	splited := strings.Split(unquoteStr, " ")
+
+	if len(splited) != 2 || splited[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(splited[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
