acc778cb35fee1ee5d26162991b07b412d10280a
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-02-02 09:02:17 +0700

    feat: permission model and permission middleware

---
+++ internal/data/permission.go
@@ -0,0 +1,55 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"slices"
+	"time"
+)
+
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	return slices.Contains(p, code)
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
+	query := `
+		SELECT permissions.code
+		FROM permissions
+		INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id INNER JOIN users ON users_permissions.user_id = users.id
+		WHERE users.id = $1
+	`
+
+	args := []any{userID}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var permissions Permissions
+	for rows.Next() {
+		var permission string
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+		permissions = append(permissions, permission)
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return permissions, nil
+
+}
