f694928f965b351e930efb9a92ec43fe8989c7ae
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-03-30 10:58:30 +0700

    refactor: use table test

--- cmd/api/middleware_test.go
+++ cmd/api/middleware_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,60 +8,49 @@ import (
 
 func TestRateLimitMiddleware(t *testing.T) {
 
-	t.Run("limit is not exceeded", func(t *testing.T) {
-		application := application{
-			config: config{
-				limiter: struct {
-					rps     float64
-					burst   int
-					enabled bool
-				}{rps: 2, burst: 4, enabled: true},
-			},
-		}
+	application := application{
+		config: config{
+			limiter: struct {
+				rps     float64
+				burst   int
+				enabled bool
+			}{rps: 2, burst: 4, enabled: true},
+		},
+	}
 
-		responseRecorder := httptest.NewRecorder()
+	cases := []struct {
+		name         string
+		expect       int
+		noOfRequests int
+	}{
+		{
+			"limit is not exceeded",
+			http.StatusOK,
+			2,
+		},
+		{
+			"limit is exceeded",
+			http.StatusTooManyRequests,
+			5,
+		},
+	}
 
-		next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, []byte("OK"))
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			next := http.HandlerFunc(application.healthCheckHandler)
+			handler := application.rateLimit(next)
+
+			var actual int
+			for range c.noOfRequests {
+				request := httptest.NewRequest(http.MethodGet, "/v1/healthcheck", nil)
+				responseRecorder := httptest.NewRecorder()
+				handler.ServeHTTP(responseRecorder, request)
+				actual = responseRecorder.Code
+			}
+
+			if actual != c.expect {
+				t.Errorf("expected status code %d but got %d", c.expect, actual)
+			}
 		})
-
-		for range 2 {
-			request := httptest.NewRequest(http.MethodGet, "/v1/healthcheck", nil)
-			application.rateLimit(next).ServeHTTP(responseRecorder, request)
-		}
-
-		if responseRecorder.Code != http.StatusOK {
-			t.Errorf("expected status code %d but got %d", http.StatusOK, responseRecorder.Code)
-		}
-	})
-
-	t.Run("limit is exceeded", func(t *testing.T) {
-		application := application{
-			config: config{
-				limiter: struct {
-					rps     float64
-					burst   int
-					enabled bool
-				}{rps: 2, burst: 4, enabled: true},
-			},
-		}
-
-		next := http.HandlerFunc(application.healthCheckHandler)
-
-		handler := application.rateLimit(next)
-		var expected int
-		for range 5 {
-			// need to put inside loop to create a new recorder for each request. otherwise the recorder will be closed after the first request and return 200 status code for subsequence requests.
-			responseRecorder := httptest.NewRecorder()
-			request := httptest.NewRequest(http.MethodGet, "/v1/healthcheck", nil)
-			handler.ServeHTTP(responseRecorder, request)
-			expected = responseRecorder.Code
-		}
-
-		if expected != http.StatusTooManyRequests {
-			t.Errorf("expected status code %d but got %d", http.StatusTooManyRequests, expected)
-		}
-
-	})
-
+	}
 }
